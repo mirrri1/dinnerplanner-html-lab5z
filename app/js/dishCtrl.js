62f7b539aa1254697a1709b54ae4e170e9f234fe
desi83 <desireefredriksson@live.se>
2016-05-26 14:27:56 +0200

    Dishes

--- app/js/dishCtrl.js
+++ app/js/dishCtrl.js
@@ -5,5 +5,36 @@ dinnerPlannerApp.controller('DishCtrl', function ($scope,$routeParams,Dinner) {
   // TODO in Lab 5: you need to get the dish according to the routing parameter
   // $routingParams.paramName
   // Check the app.js to figure out what is the paramName in this case
-  
+	var loaded = false
+	var dishID = $routeParams.dishId
+	// $scope.dish = Dinner.Dish.get({id:dishID})
+
+
+  	$scope.getDishPrice = function(){
+  		if (loaded == true){
+    		return Dinner.getDishPrice($scope.dish);
+		}
+		return $scope.status
+	}
+
+	$scope.getNumberOfGuests = function() {
+    	return Dinner.getNumberOfGuests();
+	}
+
+	$scope.addDishToMenu = function() {
+    	Dinner.addDishToMenu($scope.dish);
+  	}
+
+  	$scope.loadDish = function() {
+	   $scope.status = "Loading...";
+	   loaded = false;
+	   Dinner.Dish.get({id:dishID},function(data){
+	     $scope.dish=data;
+	     $scope.status = "Done loading"
+	   	 loaded = true},function(data){
+	     	$scope.status = "There was an error";
+	   });
+ 	}
+
+ 	$scope.loadDish()
 });
